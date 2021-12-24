3d59e21c0340e20a8a3fb16a51dc35f02a7301ae
Chris Arnault <chris.arnault.1@gmail.com>
2021-12-24 13:55:01 +0100

    GPIO/test.py blink a diod

--- GPIO/test.py
+++ GPIO/test.py
@@ -1,18 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
-// L’option GPIO.BCM signifie que nous faisons référence aux broches par le numéro “Broadcom SOC channel”, ce sont les numéros après “GPIO”
+# L’option GPIO.BCM signifie que nous faisons référence aux broches par le numéro “Broadcom SOC channel”,
+# ce sont les numéros après “GPIO”
 GPIO.setmode(GPIO.BCM)
 
-// Nous l’utilisons ceci pour éviter les messages d’avertissement car nous ne terminons pas correctement la connexion GPIO lorsque nous interrompons le programme
+# Nous l’utilisons ceci pour éviter les messages d’avertissement
+# car nous ne terminons pas correctement la connexion GPIO lorsque nous interrompons le programme
 GPIO.setwarnings(False)
 
-// nous définissons la broche LED (= 21) comme une broche de sortie
+# nous définissons la broche LED (= 21) comme une broche de sortie
 LED = 21
 
 ledState = False
 
-// Pour attribuer la valeur de la variable “ledState” à la broche. Vrai = 3.3V et Faux = 0V
+# Pour attribuer la valeur de la variable “ledState” à la broche. Vrai = 3.3V et Faux = 0V
 GPIO.setup(LED, GPIO.OUT)
 
 while True:
